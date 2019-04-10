ba024854ca702a073463b90edb05411a5bb2c10e
Cameron Farrier <48474999+Cameron-FM@users.noreply.github.com>
2019-04-10 01:00:18 +0100

    Created nlp.js V1
    Created a dictionary of 1:1 conversions in the first of the NLP script

---
+++ nlp.js
@@ -0,0 +1,30 @@
+//<script src="https://unpkg.com/compromise@latest/builds/compromise.min.js"></script>
+
+//One to One ratio
+let oneCupToGrams = {
+  flour : {
+    plainFlour: "120",
+    glutenFreeFlour : "156",
+    Cornflour: "100", //Corn Starch
+  },
+  sugar: {
+    brownSugar: "200", //Ligh Brown Sugar or Muscovado Sugar
+    casterSugar : "225", //Superfine Sugar
+    icingSugar: "100", //Confectioners Sugar or Powdered Sugar
+    granulatedSugar: "100",
+  },
+  salt : "273",
+  cocoaPowder : "100",
+  chocolateChips : "150",
+  butter : "255",
+  honey : "340",
+  treacle : "325", //Molasses
+  goldenSyrup : "340", //Corn Syrup
+  jam : "325", //Jelly
+  peanutButter : "250",
+  creamCheese: "120", //Soft Cheese
+  buttermilk : "225",
+  coconut : "100", //Shredded or Desiccated
+  oats: "140",
+  raisins: "150", //Sultanas
+};
